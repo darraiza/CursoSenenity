83649d220070d66f634df4045928f941facc6c35
Doris <d.arraiza.sixto@gmail.com>
2022-07-13 18:05:01 -0500

    comentar tag

--- src/test/java/com/automation/Runner.java
+++ src/test/java/com/automation/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty"}
         ,features = "src/test/resources/features"
         ,glue = "com.automation.web.step"
-        //,tags = "@Cabinas"
-        ,tags = "@PeruRail"
+        ,tags = "@Cabinas"
+        //,tags = "@PeruRail"
 )
 public class Runner {}
